b9c2f5dd7016a57575861d27fcdde5825e517e35
Simon Mandlik <simon.mandlik@gmail.com>
2018-07-24 13:25:52 +0200

    better paddedprint

--- src/Mill.jl
+++ src/Mill.jl
@@ -1,31 +1,26 @@
 __precompile__()
 module Mill
-using JSON 
+using JSON
 using Adapt
 using MLDataPattern
 
 VecOrRange = Union{UnitRange{Int},V where V<:AbstractVector}
 
-function paddedprint(io,s,padding)
-	for _ in 1:padding
-	    print(io, ' ')
-	end
-  print(io,s)
-end
+paddedprint(io,s,padding) = print(io, repeat(" ", padding), s)
 
-Bags = Vector{UnitRange{Int64}}
+const Bags = Vector{UnitRange{Int64}}
 
 """
-		function length2bags(ls::Vector{Int}) 
+		function length2bags(ls::Vector{Int})
 
-		creates ranges of bags from a vector of bag lengths 
+		creates ranges of bags from a vector of bag lengths
 
 		```juliadoc
 		julia> length2bags([1,3,2])
 		[1:1,2:4,5:6]
 		```
 """
-function length2bags(ls::Vector{Int}) 
+function length2bags(ls::Vector{Int})
 	ls = vcat([0],cumsum(ls))
 	bags = map(i -> i[1]+1:i[2],zip(ls[1:end-1],ls[2:end]))
 	map(b -> isempty(b) ? (0:-1) : b,bags)
@@ -36,5 +31,5 @@ include("modelnode.jl")
 include("aggregation.jl")
 
 
-export DataNode
+export DataNode, ModelNode, AggregationNode
 end
