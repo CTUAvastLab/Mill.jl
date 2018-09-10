d20cd2023f5d594e5d78490300ed254226fa3f72
Simon Mandlik <simon.mandlik@gmail.com>
2018-09-10 13:21:00 +0200

    restructuring

--- src/aggregation/aggregation.jl
+++ src/aggregation/aggregation.jl
@@ -1,5 +1,5 @@
-include("aggregation/segmented_mean.jl")
-include("aggregation/segmented_max.jl")
+include("segmented_mean.jl")
+include("segmented_max.jl")
 
 aggregation_vcat(fs...) = (args...) -> vcat([f(args...) for f in fs]...)
 segmented_meanmax = aggregation_vcat(segmented_mean, segmented_max)
