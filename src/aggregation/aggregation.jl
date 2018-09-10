11f741ed731756eea6ec6ef37ad89727f0b9e597
Simon Mandlik <simon.mandlik@gmail.com>
2018-09-10 16:43:00 +0200

    aggregation vcat

--- src/aggregation/aggregation.jl
+++ src/aggregation/aggregation.jl
@@ -1,5 +1,16 @@
+using Combinatorics: permutations
+using IterTools: subsets
+
 include("segmented_mean.jl")
 include("segmented_max.jl")
 
 aggregation_vcat(fs...) = (args...) -> vcat([f(args...) for f in fs]...)
-segmented_meanmax = aggregation_vcat(segmented_mean, segmented_max)
+
+const AGG = [:max, :mean]
+
+# generate all possible combinations
+for s in subsets(AGG)
+    length(s) > 1 && for p in permutations(s)
+        @eval $(Symbol(string("segmented_", p...))) = aggregation_vcat($(map(a -> Symbol(string("segmented_", a)), p)...))
+    end
+end
--- test/aggregation.jl
+++ test/aggregation.jl
@@ -2,6 +2,7 @@ using Flux
 import Mill: segmented_mean
 import Mill: segmented_max
 import Mill: segmented_meanmax
+import Mill: segmented_maxmean
 import Flux.Tracker: gradcheck
 
 x1 = randn(2,10)
@@ -9,7 +10,6 @@ x2 = Matrix{Float64}(reshape(1:12, 2, 6))
 bags1 = [1:5,6:10]
 bags2 = [1:1, 2:3, 4:6]
 w = [1, 1/2, 1/2, 1/8, 1/3, 13/24]
-
 
 @testset "aggregation grad check" begin
 	@test gradcheck(x -> sum(segmented_mean(x, [1:4])), randn(4, 4))
@@ -19,16 +19,20 @@ w = [1, 1/2, 1/2, 1/8, 1/3, 13/24]
 	@test gradcheck(x -> sum(segmented_max(x, [1:4, 5:5, 6:8])), randn(4, 8))
 	@test gradcheck(x -> sum(segmented_meanmax(x, [1:4])), randn(4, 4))
 	@test gradcheck(x -> sum(segmented_meanmax(x, [1:4, 5:5, 6:8])), randn(4, 8))
-	#
+	@test gradcheck(x -> sum(segmented_maxmean(x, [1:4])), randn(4, 4))
+	@test gradcheck(x -> sum(segmented_maxmean(x, [1:4, 5:5, 6:8])), randn(4, 8))
+
 	for g in [	x -> sum(segmented_mean(x, bags1)),
 				x -> sum(segmented_max(x, bags1)),
 				x -> sum(segmented_meanmax(x, bags1)),
+				x -> sum(segmented_maxmean(x, bags1)),
 		]
 		@test gradcheck(g, x1)
 	end
 	for g in [  x -> sum(segmented_mean(x, bags2, w)),
 				x -> sum(segmented_max(x, bags2, w)),
-				x -> sum(segmented_meanmax(x, bags2, w))
+				x -> sum(segmented_meanmax(x, bags2, w)),
+				x -> sum(segmented_maxmean(x, bags2, w))
 		]
 		@test gradcheck(g, x2)
 	end
@@ -38,7 +42,9 @@ end
 	@test segmented_mean(x2, bags2) ≈ [1.0 4.0 9.0; 2.0 5.0 10.0]
 	@test segmented_max(x2, bags2) ≈ [1.0 5.0 11.0; 2.0 6.0 12.0]
 	@test segmented_meanmax(x2, bags2) ≈ cat(1, segmented_mean(x2, bags2), segmented_max(x2, bags2))
+	@test segmented_maxmean(x2, bags2) ≈ cat(1, segmented_max(x2, bags2), segmented_mean(x2, bags2))
 	@test segmented_mean(x2, bags2, w) ≈ [1.0 4.0 236/24; 2.0 5.0 260/24]
 	@test segmented_max(x2, bags2, w) ≈ segmented_max(x2, bags2)
 	@test segmented_meanmax(x2, bags2, w) ≈ cat(1, segmented_mean(x2, bags2, w), segmented_max(x2, bags2, w))
+	@test segmented_maxmean(x2, bags2, w) ≈ cat(1, segmented_max(x2, bags2, w), segmented_mean(x2, bags2, w))
 end
