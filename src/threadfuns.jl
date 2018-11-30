2eba57a3f5f359000f0f95738e1505e809a705b3
pevnak <pevnak@gmail.com>
2018-11-30 12:05:09 +0100

    added forgeted threadfun.jl

---
+++ src/threadfuns.jl
@@ -0,0 +1,15 @@
+"""
+		getrange(n)
+
+		returns block of indices for a particular thred
+"""
+function getrange(n)
+    tid = Threads.threadid()
+    nt = Threads.nthreads()
+    d , r = divrem(n, nt)
+    from = (tid - 1) * d + min(r, tid - 1) + 1
+    to = from + d - 1 + (tid ≤ r ? 1 : 0)
+    from:to
+end
+
+subsetof(bags) = bags[getrange(length(bags))]
