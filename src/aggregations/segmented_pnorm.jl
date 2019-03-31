184cfebb5edd37bd16d0a294d3877c07e9add198
Simon Mandlik <simon.mandlik@gmail.com>
2019-03-31 11:04:43 +0200

    removed unnecessary print

--- src/aggregations/segmented_pnorm.jl
+++ src/aggregations/segmented_pnorm.jl
@@ -132,14 +132,6 @@ end
     end
 
     return_rule = Expr(:return, return_tuple)
-    return_rule = quote
-        @show dc
-        @show dp .* σ.(ρ)
-        $return_rule
-    end
-    x =  complete_body(init_rule, empty_bag_update_rule, init_bag_rule, mask_rule,
-                         bag_update_rule, after_bag_rule, return_rule)
-    @show x
     return complete_body(init_rule, empty_bag_update_rule, init_bag_rule, mask_rule,
                          bag_update_rule, after_bag_rule, return_rule)
 end
