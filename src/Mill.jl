943d15b6c91f8da1b45481d515cace51e53ca85a
Simon Mandlik <simon.mandlik@gmail.com>
2020-02-27 17:05:14 +0100

    transforming to HierarchicalUtils.jl

--- src/Mill.jl
+++ src/Mill.jl
@@ -1,6 +1,6 @@
 module Mill
 
-using Flux, MLDataPattern, SparseArrays, Statistics, Combinatorics, Zygote
+using Flux, MLDataPattern, SparseArrays, Statistics, Combinatorics, Zygote, HierarchicalUtils
 using Zygote: @adjoint
 import Base.reduce
 
@@ -38,75 +38,58 @@ export reflectinmodel
 include("conv.jl")
 export bagconv, BagConv
 
-# TODO delete
-include("traversal_encoding.jl")
-
 include("bagchain.jl")
 export BagChain
 
 include("replacein.jl")
 export replacein
 
-# TODO extract to new package
-include("HierarchicalUtils/src/HierarchicalUtils.jl")
-import .HierarchicalUtils: print_tree, head_string, children_string, children, nchildren, NodeType, LeafNode, InnerNode
+# TODO upravit Mill + jeho dokumentaci
+import HierarchicalUtils: NodeType, treerepr, childrenfield, child
 
-# TODO leafes by mely mit automaticky zadefinovane vlastnosti
-# TODO aggregation function also MillModel?
 Base.show(io::IO, ::MIME"text/plain", m::Union{AbstractNode, MillModel, AggregationFunction}) = print_tree(io, m, trunc_level=2, trav=false)
 Base.show(io::IO, ::T) where T <: Union{AbstractNode, MillModel, AggregationFunction} = show(io, Base.typename(T))
-
-# TODO
 Base.show(io::IO, n::NGramMatrix) = (print(io, "NGramMatrix($(n.b), $(n.m))"); show(io, n.s))
-Base.show(io::IO, ::MIME{Symbol("text/plain")}, n::NGramMatrix) = Base.show(io, n)
+Base.show(io::IO, ::MIME"text/plain", n::NGramMatrix) = Base.show(io, n)
 
 NodeType(::Type{<:Union{ArrayNode, ArrayModel, Missing}}) = LeafNode()
 NodeType(::Type{<:AggregationFunction}) = LeafNode()
 NodeType(::Type{<:AbstractNode}) = InnerNode()
 NodeType(::Type{<:MillModel}) = InnerNode()
 
-# TODO upravit Mill + jeho dokumentaci
-# # TODO delete traversal
-# TODO delete this and extract to a new package
-import .HierarchicalUtils: COLORS, paddedprint
+treerepr(::Missing) = "∅"
+treerepr(n::ArrayNode) = "ArrayNode$(size(n.data))"
+treerepr(n::BagNode) = "BagNode$(size(n.data))"
+treerepr(n::BagNode) = "BagNode with $(length(n.bags)) bag(s)"
+treerepr(n::WeightedBagNode) = "WeightedNode with $(length(n.bags)) bag(s) and weights Σw = $(sum(n.weights))"
 
-head_string(::Missing) = "∅"
-# tail_string(x::Missing) = ""
-
-head_string(n::ArrayNode) = "ArrayNode$(size(n.data))"
-# tail_string(x::ArrayNode) = ""
-
-head_string(n::BagNode) = "BagNode$(size(n.data))"
-head_string(n::BagNode) = "BagNode with $(length(n.bags)) bag(s)"
-head_string(n::WeightedBagNode) = "WeightedNode with $(length(n.bags)) bag(s) and weights Σw = $(sum(n.weights))"
-# tail_string(::AbstractBagNode) = ""
 children_string(::AbstractBagNode) = [""]
 children(n::AbstractBagNode) = (n.data,)
 nchildren(::AbstractBagNode) = 1
 
 key_labels(data::NamedTuple) = 
 key_labels(data) = ["" for _ in 1:length(data)]
-head_string(n::TreeNode) = "TreeNode"
+treerepr(n::TreeNode) = "TreeNode"
 # tail_string(n::TreeNode) = ""
 children_string(n::TreeNode{<:NamedTuple}) = ["$k: " for k in keys(n.data)]
 children_string(n::TreeNode) = ["" for k in keys(n.data)]
 children(n::TreeNode) = n.data
 nchildren(n::TreeNode) = length(n.data)
 
-head_string(n::ArrayModel) = "ArrayModel($(n.m))"
+treerepr(n::ArrayModel) = "ArrayModel($(n.m))"
 # tail_string(x::ArrayModel) = ""
 
-head_string(n::T) where T <: AggregationFunction = "$(T.name)($(length(n.C)))"
-head_string(a::Aggregation{N}) where N = "⟨" * join(head_string(f) for f in a.fs ", ") * "⟩"
+treerepr(n::T) where T <: AggregationFunction = "$(T.name)($(length(n.C)))"
+treerepr(a::Aggregation{N}) where N = "⟨" * join(treerepr(f) for f in a.fs ", ") * "⟩"
 # tail_string(::AggregationFunction) = ""
 
-head_string(n::BagModel) = "BagModel"
+treerepr(n::BagModel) = "BagModel"
 # tail_string(::BagModel) = ""
 children_string(::BagModel) = ["", "", ""]
 children(n::BagModel) = (n.im, n.a, n.bm)
 nchildren(::BagModel) = 3
 
-head_string(n::ProductModel) = "ProductModel ↦ $(head_string(n.m))"
+treerepr(n::ProductModel) = "ProductModel ↦ $(treerepr(n.m))"
 # tail_string(n::ProductModel) = " ) ↦  $(n.m)"
 children_string(n::ProductModel{<:NamedTuple}) = ["$k: " for k in keys(n.ms)]
 children_string(n::ProductModel) = ["" for k in keys(n.ms)]
