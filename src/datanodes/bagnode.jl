c369b807716544106fe70371cb71e8a524f69708
Pevnak <pevnak@gmail.com>
2020-03-19 14:12:51 +0100

    fixed printing

--- src/datanodes/bagnode.jl
+++ src/datanodes/bagnode.jl
@@ -34,8 +34,12 @@ removeinstances(a::BagNode, mask) = BagNode(subset(a.data, findall(mask)), adjus
 
 function dsprint(io::IO, n::BagNode{T}; pad=[], s="", tr=false) where T
     c = COLORS[(length(pad)%length(COLORS))+1]
-    m = T <: Missing ? " missing " : " "
-    paddedprint(io,"BagNode with $(length(n.bags))$(m)bag(s)$(tr_repr(s, tr))\n", color=c)
-    paddedprint(io, "  └── ", color=c, pad=pad)
-    dsprint(io, n.data, pad = [pad; (c, "      ")], s=s * encode(1, 1), tr=tr)
+    if ismissing(n.data) || nobs(n.data) == 0
+        paddedprint(io,"BagNode with $(length(n.bags)) empty bag(s)$(tr_repr(s, tr))\n", color=c)
+        paddedprint(io, "  └── ∅", color=c, pad=pad)
+    else
+        paddedprint(io,"BagNode with $(length(n.bags)) bag(s)$(tr_repr(s, tr))\n", color=c)
+        paddedprint(io, "  └── ", color=c, pad=pad)
+        dsprint(io, n.data, pad = [pad; (c, "      ")], s=s * encode(1, 1), tr=tr)
+    end
 end
