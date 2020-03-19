fcc0148f958d816f30afc491ab8a897f9dc29382
Pevnak <pevnak@gmail.com>
2020-03-19 14:09:34 +0100

    changed behavior of getindex in BagNode of Mill and added tests

--- src/datanodes/bagnode.jl
+++ src/datanodes/bagnode.jl
@@ -17,7 +17,7 @@ mapdata(f, x::BagNode) = BagNode(mapdata(f, x.data), x.bags, x.metadata)
 
 function Base.getindex(x::BagNode, i::VecOrRange)
     nb, ii = remapbag(x.bags, i)
-    isempty(ii) && return(BagNode(missing, nb, nothing))
+    # isempty(ii) && return(BagNode(missing, nb, nothing))
     BagNode(subset(x.data,ii), nb, subset(x.metadata, i))
 end
 
--- test/modelnode.jl
+++ test/modelnode.jl
@@ -52,3 +52,35 @@ end
     @test typeof(m.bm) <: ArrayModel
     @test eltype(m(x).data) == Float32
 end
+
+@testset "testing nested bag model" begin
+    bn = BagNode(ArrayNode(randn(Float32, 2, 8)), [1:1, 2:2, 3:6, 7:8])
+    x = BagNode(bn, [1:2, 3:4])
+    m = reflectinmodel(x, layerbuilder)
+    @test size(m(x).data) == (2, 2)
+    @test typeof(m) <: BagModel
+    @test typeof(m.im) <: BagModel
+    @test typeof(m.im.im) <: ArrayModel
+    @test typeof(m.im.bm) <: ArrayModel
+    @test typeof(m.bm) <: ArrayModel
+    @test eltype(m(x).data) == Float32
+
+    a = BagNode(BagNode(ArrayNode(randn(2,2)),[1:2]),[1:1])
+    b = BagNode(missing,[0:-1])
+    c = BagNode(a.data[1:0], [0:-1])
+    m = reflectinmodel(a, d -> Dense(d,2), d -> SegmentedMeanMax(d))
+    abc = catobs(a, b, c)
+    bca = catobs(b, c, a)
+    ma = m(a).data
+    mb = m(b).data
+    mc = m(c).data
+    mabc = m(abc).data
+    mbca = m(bca).data
+    @test mabc[:,1] ≈ ma
+    @test mabc[:,2] ≈ mb
+    @test mabc[:,3] ≈ mc
+    @test mba[:,1] ≈ mb
+    @test mbca[:,1] ≈ mb
+    @test mbca[:,2] ≈ mc
+    @test mbca[:,3] ≈ ma
+end
