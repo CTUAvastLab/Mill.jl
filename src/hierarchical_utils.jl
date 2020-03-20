9d4dcfe6b7e4788b81a7869d82a548ad5218f438
Simon Mandlik <simon.mandlik@gmail.com>
2020-03-20 10:45:03 +0100

    missing bag node printing

--- src/hierarchical_utils.jl
+++ src/hierarchical_utils.jl
@@ -1,14 +1,16 @@
 import HierarchicalUtils: NodeType, noderepr, childrenfields, children
 
-NodeType(::Type{<:Union{ArrayNode, ArrayModel, BagNode{Missing}}}) = LeafNode()
+NodeType(::Type{<:Union{Missing, ArrayNode, ArrayModel}}) = LeafNode()
 NodeType(::Type{<:AbstractNode}) = InnerNode()
 NodeType(::Type{<:AbstractBagNode}) = SingletonNode()
 NodeType(::Type{<:BagModel}) = SingletonNode()
 NodeType(::Type{<:MillModel}) = InnerNode()
 
+noderepr(::Missing) = "∅"
 noderepr(n::ArrayNode) = "ArrayNode$(size(n.data))"
 noderepr(n::ArrayModel) = "ArrayModel($(n.m))"
 noderepr(n::BagNode) = "BagNode with $(length(n.bags)) bag(s)"
+noderepr(n::BagNode{Missing}) = "BagNode with $(length(n.bags)) empty bag(s)"
 noderepr(n::BagModel) = "BagModel ↦ $(repr("text/plain", n.a)) ↦ $(repr("text/plain", n.bm))"
 noderepr(n::WeightedBagNode) = "WeightedNode with $(length(n.bags)) bag(s) and weights Σw = $(sum(n.weights))"
 noderepr(n::AbstractTreeNode) = "TreeNode"
