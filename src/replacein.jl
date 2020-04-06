f5b47c93c792687e124887591b865421639dc621
Simon Mandlik <simon.mandlik@gmail.com>
2020-04-06 19:49:35 +0200

    new replacein version, added tests

--- src/replacein.jl
+++ src/replacein.jl
@@ -1,12 +1,19 @@
 replacein(x, oldnode, newnode) = x
 replacein(x::Tuple, oldnode, newnode) = tuple([replacein(m, oldnode, newnode) for m in x]...)
 replacein(x::NamedTuple, oldnode, newnode) = (;[k => replacein(x[k], oldnode, newnode) for k in keys(x)]...)
-
-for T in [ArrayNode, ArrayModel, BagModel, BagNode, ProductNode, ProductModel]
-    @eval begin
-        function replacein(x::$T, oldnode, newnode)
-            x == oldnode && return(newnode)
-            $T(map(f -> replacein(getproperty(x, f), oldnode, newnode), fieldnames($T))...)
-        end
-    end
+function replacein(x::T, oldnode, newnode) where {T<:Union{AbstractNode, AbstractMillModel}}
+    x == oldnode && return(newnode)
+    fields = map(f -> replacein(getproperty(x, f), oldnode, newnode), fieldnames(T))
+    n = nameof(T)
+    p = parentmodule(T)
+    eval(:($p.$n))(fields...)
 end
+
+# TODO
+# @generated function replacein(x::T, oldnode, newnode) where T <: Union{AbstractNode, AbstractMillModel}
+#     return quote
+#         x === oldnode && return newnode
+#         fields = map(f -> replacein(getproperty(x, f), oldnode, newnode), $(fieldnames(T)))
+#         # $(T)(fields...)
+#     end
+# end
