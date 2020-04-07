15c8d35ad691dbb68b5f9f66464fe221aaee8860
Pevnak <pevnak@gmail.com>
2020-04-07 21:06:44 +0200

    terse printing

--- src/Mill.jl
+++ src/Mill.jl
@@ -57,6 +57,20 @@ Base.show(io::IO, ::T) where T <: Union{AbstractNode, AbstractMillModel, Aggrega
 Base.show(io::IO, ::MIME"text/plain", n::Union{AbstractNode, AbstractMillModel}) = HierarchicalUtils.printtree(io, n; trunc_level=2)
 Base.getindex(n::Union{AbstractNode, AbstractMillModel}, i::AbstractString) = HierarchicalUtils.walk(n, i)
 
+
+const _terseprint = Ref(true)
+
+function terseprint(a)
+    _terseprint[] = a
+end
+
+function Base.show(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
+	if _terseprint[]
+		print(io, "$(x.name){…}")
+	else
+		Base.show_datatype(io, x)
+	end
+end
 export printtree
 
 end
