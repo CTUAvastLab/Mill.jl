4bf8afadb759f2c1ad4ac0aec8d904f3a7daaabf
Matěj Račinský <matej.racinsky@avast.com>
2020-04-30 20:21:42 +0200

    finally fixed fucking terseprint

--- src/Mill.jl
+++ src/Mill.jl
@@ -71,21 +71,26 @@ end
 
 function Base.show(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
 	if _terseprint[]
-		print(io, "$(x.name){…}")
-		return
+		if !hasproperty(x, :name) && hasproperty(x, :body)
+			print(io, "$(x.body.name){…}")
+			return
+		else
+			print(io, "$(x.name){…}")
+			return
+		end
 	# basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
     elseif x isa DataType
         Base.show_datatype(io, x)
         return
     elseif x isa Union
         print(io, "Union")
-        Base.show_delim_array(io, uniontypes(x), '{', ',', '}', false)
+        Base.show_delim_array(io, Base.uniontypes(x), '{', ',', '}', false)
         return
     end
     x::UnionAll
 
     if Base.print_without_params(x)
-        return show(io, unwrap_unionall(x).name)
+        return show(io, Base.unwrap_unionall(x).name)
     end
 
     if x.var.name === :_ || Base.io_has_tvar_name(io, x.var.name, x)
@@ -103,7 +108,9 @@ function Base.show(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMill
 
     show(IOContext(io, :unionall_env => x.var), x.body)
     print(io, " where ")
-    show(io, x.var)end
+    show(io, x.var)
+end
+
 export printtree
 
 end
