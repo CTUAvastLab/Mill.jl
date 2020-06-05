21b9c0d1cb7e967ead88cf99ffc1d3570fa4009d
Matěj Račinský <matej.racinsky@avast.com>
2020-06-05 14:26:40 +0200

    fixed printtree

--- test/hierarchical_utils.jl
+++ test/hierarchical_utils.jl
@@ -132,14 +132,14 @@ end
     printtree(buf, n2, trav=true)
     str_repr = String(take!(buf))
     @test str_repr ==
-    """
-    ProductNode [""]
-    ├── ProductNode ["E"]
-    │     ├─── b: BagNode with 2 bag(s) ["I"]
-    │     │         └── ArrayNode(3, 4) ["K"]
-    │     └── wb: WeightedNode with 2 bag(s) and weights Σw = 11 ["M"]
-    │               └── ArrayNode(17, 4) ["O"]
-    └── ArrayNode(10, 2) ["U"]"""
+"""
+ProductNode [""]
+  ├── ProductNode ["E"]
+  │     ├─── b: BagNode with 2 bag(s) ["I"]
+  │     │         └── ArrayNode(3, 4) ["K"]
+  │     └── wb: WeightedNode with 2 bag(s) and weights Σw = 11 ["M"]
+  │               └── ArrayNode(17, 4) ["O"]
+  └── ArrayNode(10, 2) ["U"]"""
 
   buf = IOBuffer()
   printtree(buf, n2m, trav=true)
