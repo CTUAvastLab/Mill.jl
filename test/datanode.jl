835f2bdd13af70a56e0f8a5c94e76307de1c51a8
Matěj Račinský <matej.racinsky@avast.com>
2020-10-16 09:28:15 +0200

    more specific failure of tests

--- test/datanode.jl
+++ test/datanode.jl
@@ -315,17 +315,20 @@ end
     	@show T
     end
     t = UnionAll(TypeVar(:t), LazyNode)
+    u = typeof(LazyNode(:oh_hi, ["Mark"]))
 
     orig_terse = Mill._terseprint[]
 
     Mill.terseprint(true)
     @test occursin("(ds::LazyNode{…}) where T<:Symbol", buf_show(methods(experiment)))
     @test buf_show(t) == "LazyNode{…}"
+    @test buf_show(u) == "LazyNode{…}"
 
     # extremely weird behavior, see https://github.com/pevnak/Mill.jl/issues/45
 	Mill.terseprint(false)
-    @test_broken startswith("(ds::LazyNode{T,D} where D) where T<:Symbol", buf_show(methods(experiment)))
-    @test_broken buf_show(t) == "LazyNode{T} where T<:Symbol"
+    @test_throws ErrorException startswith("(ds::LazyNode{T,D} where D) where T<:Symbol", buf_show(methods(experiment)))
+    @test buf_show(t) == "LazyNode"
+    @test buf_show(u) == "LazyNode{:oh_hi,Array{String,1}}"
 
     Mill.terseprint(orig_terse)
 
