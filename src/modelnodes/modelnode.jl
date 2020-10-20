a73405e8df97440e91b85de7637a5ba0d367cc3b
Simon Mandlik <simon.mandlik@gmail.com>
2020-10-20 11:34:26 +0200

    single scalar as identity

--- src/modelnodes/modelnode.jl
+++ src/modelnodes/modelnode.jl
@@ -10,20 +10,22 @@ include("productmodel.jl")
 include("missingmodel.jl")
 include("lazymodel.jl")
 
-reflectinmodel(x, db=d->Flux.Dense(d, 10), da=d->SegmentedMean(d); b = Dict(), a = Dict(),
-               single_key_identity=true) = _reflectinmodel(x, db, da, b, a, "", single_key_identity)[1]
+function reflectinmodel(x, db=d->Flux.Dense(d, 10), da=d->SegmentedMean(d); b = Dict(), a = Dict(),
+               single_key_identity=true, single_scalar_identity=true)
+    _reflectinmodel(x, db, da, b, a, "", single_key_identity, single_scalar_identity)[1]
+end
 
-function _reflectinmodel(x::AbstractBagNode, db, da, b, a, s)
-    im, d = _reflectinmodel(x.data, db, da, b, a, s * encode(1, 1))
+function _reflectinmodel(x::AbstractBagNode, db, da, b, a, s, ski, sci)
+    im, d = _reflectinmodel(x.data, db, da, b, a, s * encode(1, 1), ski, sci)
     c = stringify(s)
     agg = c in keys(a) ? a[c](d) : da(d)
-    bm, d = _reflectinmodel(BagModel(im, agg)(x), db, da, b, a, s)
+    bm, d = _reflectinmodel(BagModel(im, agg)(x), db, da, b, a, s, ski, sci)
     BagModel(im, agg, bm), d
 end
 
-function _reflectinmodel(x::AbstractProductNode, db, da, b, a, s)
+function _reflectinmodel(x::AbstractProductNode, db, da, b, a, s, ski, sci)
     n = length(x.data)
-    ms = [_reflectinmodel(xx, db, da, b, a, s * encode(i, n), ski) for (i, xx) in enumerate(x.data)]
+    ms = [_reflectinmodel(xx, db, da, b, a, s * encode(i, n), ski, sci) for (i, xx) in enumerate(x.data)]
     if ski && n == 1
         # use for julia 1.5 and higher
         # im, d = only(ms)
@@ -31,15 +33,15 @@ function _reflectinmodel(x::AbstractProductNode, db, da, b, a, s)
         ProductModel(im), d
     else
         im = tuple([i[1] for i in ms]...)
-        tm, d = _reflectinmodel(ProductModel(im)(x), db, da, b, a, s, ski)
+        tm, d = _reflectinmodel(ProductModel(im)(x), db, da, b, a, s, ski, sci)
         ProductModel(im, tm), d
     end
 end
 
-function _reflectinmodel(x::ProductNode{T,C}, db, da, b, a, s) where {T<:NamedTuple, C}
+function _reflectinmodel(x::ProductNode{T}, db, da, b, a, s, ski, sci) where T <: NamedTuple
     n = length(x.data)
     ks = keys(x.data)
-    ms = [_reflectinmodel(x.data[k], db, da, b, a, s * encode(i, n), ski) for (i, k) in enumerate(ks)]
+    ms = [_reflectinmodel(x.data[k], db, da, b, a, s * encode(i, n), ski, sci) for (i, k) in enumerate(ks)]
     if ski && n == 1
         # use for julia 1.5 and higher
         # im, d = only(ms)
@@ -48,14 +50,32 @@ function _reflectinmodel(x::ProductNode{T,C}, db, da, b, a, s) where {T<:NamedTu
         ProductModel((; ks[1]=>im)), d
     else
         im = (; (k=>v[1] for (k,v) in zip(ks, ms))...)
-        tm, d = _reflectinmodel(ProductModel(im)(x), db, da, b, a, s, ski)
+        tm, d = _reflectinmodel(ProductModel(im)(x), db, da, b, a, s, ski, sci)
         ProductModel(im, tm), d
     end
 end
 
-function _reflectinmodel(x::ArrayNode, db, da, b, a, s)
+function _reflectinmodel(x::ArrayNode, db, da, b, a, s, ski, sci)
     c = stringify(s)
-    t = c in keys(b) ? b[c](size(x.data, 1)) : db(size(x.data, 1))
+    r = size(x.data, 1)
+    if c in keys(b)
+        t = b[c](r)
+    elseif sci && r == 1
+        t = identity
+    else
+        t = db(r)
+    end
     m = ArrayModel(t)
     m, size(m(x).data, 1)
+end
+
+function _reflectinmodel(ds::LazyNode{Name}, db, da, b, a, s, ski, sci) where Name
+    pm, d = Mill._reflectinmodel(unpack2mill(ds), db, da, b, a, s * Mill.encode(1, 1), ski, sci)
+    LazyModel{Name}(pm), d
+end
+
+function _reflectinmodel(x::MissingNode, db, da, b, a, s, ski, sci)
+    im, d = _reflectinmodel(x.data, db, da, b, a, s, ski, sci)
+    θ = zeros(Float32, d)
+    MissingModel(im, θ), d
 end
