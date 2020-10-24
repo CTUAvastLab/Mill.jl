abc82faee8cd6375970f6efe64a2015610adcac4
Simon Mandlik <simon.mandlik@gmail.com>
2020-10-24 19:32:53 +0200

    imputing matrix defined to NOT be AbstractMatrix for the time being

--- src/matrices/imputing_matrix.jl
+++ src/matrices/imputing_matrix.jl
@@ -1,3 +1,5 @@
+# TODO make ImputingMatrix AbstractMatrix
+# see: https://github.com/FluxML/Zygote.jl/issues/815
 # struct ImputingMatrix{T, U <: AbstractMatrix{T}, V <: AbstractVector{T}} <: AbstractMatrix{T}
 struct ImputingMatrix{T, U <: AbstractMatrix{T}, V <: AbstractVector{T}}
     W::U
@@ -10,29 +12,38 @@ ImputingMatrix(W::AbstractMatrix{T}) where T = ImputingMatrix(W, zeros(T, size(W
 
 Flux.@forward ImputingMatrix.W Base.size, Base.getindex, Base.setindex!, Base.firstindex, Base.lastindex
 
-function Base.print_matrix(io::IO, A::ImputingMatrix)
-    println(io, "W:")
-    print_matrix(io, A.W)
-    println(io, "\nψ:")
-    print_matrix(io, A.ψ)
-end
-
-function Flux.params!(p::Params, A::ImputingMatrix, seen=IdSet())
-  A in seen && return
-  push!(seen, A)
-  push!(p, A.W, A.ψ)
-end
-
 Base.vcat(As::ImputingMatrix...) = ImputingMatrix(vcat((A.W for A in As)...), vcat((A.ψ for A in As)...))
 
-_fill_in(::AbstractVector{T}, B::AbstractVecOrMat{T}) where T = B
-function _fill_in(ψ::AbstractVector{T}, B::AbstractVecOrMat{<:MissingElement{T}}) where T
-    X = similar(B, T)
-    @inbounds for i in CartesianIndices(B)
-        X[i] = ismissing(B[i]) ? ψ[i[1]] : B[i]
-    end
-    X
+# function print_matrix(io::IO, A::ImputingMatrix)
+#     println(io, "W:")
+#     print_matrix(io, A.W)
+#     println(io, "\nψ:")
+#     print_matrix(io, A.ψ)
+# end
+
+# function Flux.params!(p::Params, A::ImputingMatrix, seen=IdSet())
+#   A in seen && return
+#   push!(seen, A)
+#   push!(p, A.W, A.ψ)
+# end
+
+# TODO define adjoint here?
+A::ImputingMatrix * B::AbstractVecOrMat = A.W * B
+A::ImputingMatrix * b::AbstractVector{<:MissingElement} = A.W * _fill_in(A.ψ, b)
+A::ImputingMatrix * B::AbstractMatrix{<:MissingElement} = A.W * _fill_in(A.ψ, B)
+
+_fill_in(ψ::AbstractVector{T}, B::AbstractVecOrMat{<:MissingElement{T}}) where T = _fill_mask(ψ, B)[1]
+function rrule(::typeof(_fill_in), ψ::AbstractVector{T}, B::AbstractVecOrMat{<:MissingElement{T}}) where T
+    X, m = _fill_mask(ψ, B)
+    _fill_in_dψ(Δ) = (dψ = deepcopy(Δ); dψ[m] .= 0; sum(dψ, dims=2))
+    _fill_in_dB(Δ) = (dB = deepcopy(Δ); dB[.!m] .= 0; dB)
+    X, Δ -> (NO_FIELDS, @thunk(_fill_in_dψ(Δ)), @thunk(_fill_in_dB(Δ)))
 end
 
-A::ImputingMatrix * B::AbstractVecOrMat{<:MissingElement{T}} where T = _imput_mult(A.W, A.ψ, B)
-_imput_mult(W, ψ, B) = W * _fill_in(ψ, B)
+function _fill_mask(ψ::AbstractVector{T}, B::AbstractVecOrMat{<:MissingElement{T}}) where T
+    m = .!ismissing.(B)
+    X = similar(B, T)
+    X .= ψ
+    X[m] = B[m]
+    X, m
+end
