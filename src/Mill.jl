06af626db6955eb124bf42ab1f8dd5baf1395fc0
Simon Mandlik <simon.mandlik@gmail.com>
2020-11-03 18:03:44 +0100

    removed terseprint related stuff

--- src/Mill.jl
+++ src/Mill.jl
@@ -23,15 +23,12 @@ import ChainRulesCore: rrule
 
 # GLOBAL SWITCHES
 const _emptyismissing = Ref(false)
-const _terseprint = Ref(true)
 const _bagcount = Ref(true)
 
 emptyismissing!(a) = _emptyismissing[] = a
-terseprint!(a) = _terseprint[] = a
 bagcount!(a) = _bagcount[] = a
 
 emptyismissing() = _emptyismissing[]
-terseprint() = _terseprint[]
 bagcount() = _bagcount[]
 
 # COMMON ALIASES
@@ -102,78 +99,5 @@ function _show_fields(io, x::T;context=:compact=>true) where T
 end
 
 Base.getindex(n::Union{AbstractNode, AbstractMillModel}, i::AbstractString) = HierarchicalUtils.walk(n, i)
-
-function base_show_terse(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
-    if hasproperty(x, :body) && !hasproperty(x.body, :name) && hasproperty(x.body, :body)
-        print(io, "$(x.body.body.name){…}")
-        return
-    elseif hasproperty(x, :body) && !hasproperty(x, :name)
-        print(io, "$(x.body.name){…}")
-        return
-    else
-        print(io, "$(x.name){…}")
-        return
-    end
-end
-
-function base_show_full(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
-    # basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
-    # specifically function show(io::IO, @nospecialize(x::Type))
-    if x isa DataType
-        Base.show_datatype(io, x)
-        return
-    elseif x isa Union
-        if x.a isa DataType && Core.Compiler.typename(x.a) === Core.Compiler.typename(DenseArray)
-            T2, N = x.a.parameters
-            if x == StridedArray{T2,N}
-                print(io, "StridedArray")
-                Base.show_delim_array(io, (T2,N), '{', ',', '}', false)
-                return
-            elseif x == StridedVecOrMat{T2}
-                print(io, "StridedVecOrMat")
-                Base.show_delim_array(io, (T2,), '{', ',', '}', false)
-                return
-            elseif StridedArray{T2,N} <: x
-                print(io, "Union")
-                Base.show_delim_array(io, vcat(StridedArray{T2,N}, Base.uniontypes(Core.Compiler.typesubtract(x, StridedArray{T2,N}))), '{', ',', '}', false)
-                return
-            end
-        end
-        print(io, "Union")
-        Base.show_delim_array(io, Base.uniontypes(x), '{', ',', '}', false)
-        return
-    end
-
-    # this type assert is behaving obscurely. When in Mill, it does not assert that LazyNode{T<:Symbol,D} where D is UnionAll, but in debugging using Debugger, it does
-    # x::UnionAll
-    if Base.print_without_params(x)
-        return show(io, Base.unwrap_unionall(x).name)
-    end
-
-    if x.var.name === :_ || Base.io_has_tvar_name(io, x.var.name, x)
-        counter = 1
-        while true
-            newname = Symbol(x.var.name, counter)
-            if !Base.io_has_tvar_name(io, newname, x)
-                newtv = TypeVar(newname, x.var.lb, x.var.ub)
-                x = UnionAll(newtv, x{newtv})
-                break
-            end
-            counter += 1
-        end
-    end
-
-    show(IOContext(io, :unionall_env => x.var), x.body)
-    print(io, " where ")
-    show(io, x.var)
-end
-
-function Base.show(io::IO, x::Type{<:Union{AbstractNode, AbstractMillModel}})
-    if _terseprint[]
-        return base_show_terse(io, x)
-    else
-        return base_show_full(io, x)
-    end
-end
 
 end
--- test/datanode.jl
+++ test/datanode.jl
@@ -252,22 +252,6 @@ end
     @test all(xs.data[1].data[1].data .== x.data[1].data[1].data)
 end
 
-@testset "testing terseprint on" begin
-    Mill.terseprint(true)
-    buf = IOBuffer()
-    show(buf, typeof([h,i]))
-    str_repr = String(take!(buf))
-    @test str_repr == "Array{ProductNode{…},1}"
-end
-
-@testset "testing terseprint off" begin
-    Mill.terseprint(false)
-    buf = IOBuffer()
-    show(buf, typeof([h,i]))
-    str_repr = String(take!(buf))
-    @test str_repr == "Array{ProductNode{T,Nothing} where T,1}"
-end
-
 @testset "testing equals and hash" begin
     a2 = deepcopy(a)
     i2 = deepcopy(i)
@@ -295,48 +279,4 @@ end
     @test hash(k) === hash(k2)
     @test hash(x1) === hash(x2)
     @test hash(x1) !== hash(x3)
-end
-
-function buf_show(data; kwargs...)
-    buf = IOBuffer()
-	Base.show(buf, data; kwargs...)
-	String(take!(buf))
-end
-
-@testset "testing more obscure terseprint things" begin
-    function experiment(ds::LazyNode{T}) where {T<:Symbol}
-    	@show ds
-    	@show T
-    end
-    a = methods(experiment)
-    a_method = a.ms[1]
-    b = getfield(a_method.sig, 2)
-    c = getfield(b, 3)
-    d = c[2]
-    e = d.body
-
-    t = UnionAll(TypeVar(:t), LazyNode)
-    u = typeof(LazyNode(:oh_hi, ["Mark"]))
-
-    orig_terse = Mill._terseprint[]
-
-    Mill.terseprint(true)
-    @test occursin("(ds::LazyNode{…}) where T<:Symbol", buf_show(methods(experiment)))
-    @test buf_show(t) == "LazyNode{…}"
-    @test buf_show(u) == "LazyNode{…}"
-    @test buf_show(d) == "LazyNode{…}"
-    @test buf_show(e) == "LazyNode{…}"
-
-    # extremely weird behavior, see https://github.com/pevnak/Mill.jl/issues/45
-	Mill.terseprint(false)
-    @test_throws ErrorException startswith("(ds::LazyNode{T,D} where D) where T<:Symbol", buf_show(methods(experiment)))
-    @test_broken buf_show(methods(experiment))
-    @test buf_show(t) == "LazyNode"
-    @test buf_show(u) == "LazyNode{:oh_hi,Array{String,1}}"
-    @test_throws ErrorException buf_show(d)
-    @test_broken buf_show(d) == "LazyNode{T<:Symbol,D}"
-	@test_throws ErrorException buf_show(e)
-    @test_broken buf_show(e) == "LazyNode{T<:Symbol,D}"
-
-    Mill.terseprint(orig_terse)
 end
