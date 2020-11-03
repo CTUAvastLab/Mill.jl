51c5036637a8c323e07f8bc7ee73e3db37eb27b7
Simon Mandlik <simon.mandlik@gmail.com>
2020-11-03 09:29:14 +0100

    io for matrix.jl

--- src/matrices/matrix.jl
+++ src/matrices/matrix.jl
@@ -24,22 +24,6 @@ include("row_imputing_matrix.jl")
 include("col_imputing_matrix.jl")
 
 const ImputingMatrix{T, C, U} = Union{RowImputingMatrix{T, C, U}, ColImputingMatrix{T, C, U}}
-
-function Base.show(io::IO, X::T) where T <: Union{ImputingMatrix, MaybeHotMatrix,
-                                             MaybeHotVector, NGramMatrix}
-    if get(io, :compact, false)
-        print(io, size(X, 1), "x", size(X, 2), " ", typename(T))
-    else
-        _show(io, X)
-    end
-end
-
-_show(io, X::ImputingMatrix) = print(io, _name(X), "Matrix(W = ", X.W, ", ψ = ", X.ψ, ")")
-_show(io, x::MaybeHotVector) = print(io, "MaybeHotVector(i = ", x.i, ", l = ", x.l, ")")
-_show(io, X::MaybeHotMatrix) = print(io, "MaybeHotMatrix(I = ", X.I, ", l = ", X.l, ")")
-_show(io, X::NGramMatrix) = print(io, "NGramMatrix(s = ", X.s, ", n = ", X.n, 
-              ", b = ", X.b, ", m = ", X.m, ")")
-
 _print_params(io::IO, A::RowImputingMatrix) = print_array(io, A.ψ |> permutedims)
 _print_params(io::IO, A::ColImputingMatrix) = print_array(io, A.ψ)
 
@@ -51,6 +35,15 @@ function print_array(io::IO, A::ImputingMatrix)
 end
 
 print_array(io::IO, A::NGramMatrix) = print_array(io, A.s)
+
+function Base.show(io::IO, @nospecialize X::T) where T <: Union{ImputingMatrix, MaybeHotMatrix,
+                                             MaybeHotVector, NGramMatrix}
+    if get(io, :compact, false)
+        print(io, size(X, 1), "x", size(X, 2), " ", nameof(T))
+    else
+        _show_fields(io, X)
+    end
+end
 
 function Flux.params!(p::Params, A::ImputingMatrix, seen=IdSet())
     A in seen && return
@@ -65,7 +58,7 @@ ColImputingDense(args...) = ColImputingDense(Dense(args...))
 
 _name(::RowImputingMatrix) = "RowImputing"
 _name(::ColImputingMatrix) = "ColImputing"
-function Base.show(io::IO, l::Dense{F, <:ImputingMatrix}) where F
+function Base.show(io::IO, @nospecialize l::Dense{F, <:ImputingMatrix}) where F
   print(io, "$(_name(l.W))Dense(", size(l.W, 2), ", ", size(l.W, 1))
   l.σ == identity || print(io, ", ", l.σ)
   print(io, ")")
