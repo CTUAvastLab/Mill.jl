6ce9a83f4ca5587f0883f923a8cf6ddbcc8f2b87
Pevnak <pevnak@gmail.com>
2020-11-04 10:58:02 +0100

    added deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [Mill]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/pevnak/Mill.jl.git",
+)
