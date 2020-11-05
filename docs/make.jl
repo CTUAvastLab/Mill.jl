2e26b406baf3d67c7e58a40a3dda08087bfbcf9f
Pevnak <pevnak@gmail.com>
2020-11-05 19:33:45 +0100

    fixed make

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,18 @@ using Mill
 makedocs(
     sitename = "Mill",
     format = Documenter.HTML(),
-    modules = [Mill]
+    modules = [Mill],
+    pages = ["Home" => "index.md",
+# Introduction to MIL, how it is different from GNN, what it is , and walktrough example
+# Advanced example mixing products and bags (suggestions welcomed)
+# Architecture of the library, parallels between Nodes and Models and minimum required functions for integration
+# Handling missing values
+# Performance Primitives (NGramStrings)
+# Conveniences (Lenses)
+# External Tools (JsonGrinder, HierarchicalUtils)
+Suggested by Jan Francu, ?extensions to Graph NN? / Dags?
+	],
+
 )
 
 deploydocs(
