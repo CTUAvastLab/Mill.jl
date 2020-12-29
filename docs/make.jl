d8f323a32085a9b444d167bb3eb72c7f09f1f1e4
Šimon Mandlík <simon.mandlik@avast.com>
2020-12-29 19:25:57 +0100

    prettyurls disabled locally

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,7 @@ makedocs(
          CitationBibliography(joinpath(@__DIR__, "references.bib")),
          sitename = "Mill.jl",
          format = Documenter.HTML(sidebar_sitename=false,
+                                  prettyurls = get(ENV, "CI", nothing) == "true",
                                   assets = ["assets/favicon.ico", "assets/custom.css"]),
          modules = [Mill],
          pages = ["Home" => ["index.md"],
@@ -23,13 +24,13 @@ makedocs(
                                              "Aggregations" => "architecture/aggregation.md",
                                              "Missing values" => "architecture/missing.md",
                                              "Custom Nodes" => "architecture/custom.md",
-                                             ],
+                                            ],
                   "Helper tools" => [
                                      "HierarichalUtils.jl" => "tools/hierarchical.md",
                                      # Conveniences (Lenses)
                                      # External Tools (JsonGrinder, HierarchicalUtils)
                                     ],
-                  ],
+                 ],
 
         )
 
