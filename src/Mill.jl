ef52a145b3ee57443bd4cb361cd4052cc2aa2781
Matěj Račinský <matej.racinsky@avast.com>
2021-09-02 23:40:03 +0200

    fixing terse printing

--- src/Mill.jl
+++ src/Mill.jl
@@ -78,15 +78,27 @@ function terseprint(a)
     _terseprint[] = a
 end
 
-function base_show_terse(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
-    while hasproperty(x, :body) && !hasproperty(x, :name)
-        x = x.body
+function with_terseprint(f::Function, a)
+    orig_val = _terseprint[]
+    _terseprint[] = a
+    f()
+    _terseprint[] = orig_val
+end
+
+function base_show_terse(io::IO, @nospecialize(x::Type))
+    # print(io, typeof(x))
+    if x isa Union
+        print(io, "Union")
+        Base.show_delim_array(io, Base.uniontypes(x), '{', ',', '}', false)
+        return
     end
-    print(io, "$(x.name){…}")
+    x = Base.unwrap_unionall(x)
+    print(io, "$(x.name.name){…}")
     return
 end
 
-function base_show_full(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
+@static if VERSION < v"1.6.0-"
+function base_show_full_1_5(io::IO, @nospecialize(x::Type))
     # basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
     # specifically function show(io::IO, @nospecialize(x::Type))
     if x isa DataType
@@ -94,18 +106,18 @@ function base_show_full(io::IO, x::Type{T}) where {T<:Union{AbstractNode,Abstrac
         return
     elseif x isa Union
         if x.a isa DataType && Core.Compiler.typename(x.a) === Core.Compiler.typename(DenseArray)
-            T2, N = x.a.parameters
-            if x == StridedArray{T2,N}
+            T, N = x.a.parameters
+            if x == StridedArray{T,N}
                 print(io, "StridedArray")
-                Base.show_delim_array(io, (T2,N), '{', ',', '}', false)
+                Base.show_delim_array(io, (T,N), '{', ',', '}', false)
                 return
-            elseif x == StridedVecOrMat{T2}
+            elseif x == StridedVecOrMat{T}
                 print(io, "StridedVecOrMat")
-                Base.show_delim_array(io, (T2,), '{', ',', '}', false)
+                Base.show_delim_array(io, (T,), '{', ',', '}', false)
                 return
-            elseif StridedArray{T2,N} <: x
+            elseif StridedArray{T,N} <: x
                 print(io, "Union")
-                Base.show_delim_array(io, vcat(StridedArray{T2,N}, Base.uniontypes(Core.Compiler.typesubtract(x, StridedArray{T2,N}))), '{', ',', '}', false)
+                Base.show_delim_array(io, vcat(StridedArray{T,N}, Base.uniontypes(Core.Compiler.typesubtract(x, StridedArray{T,N}))), '{', ',', '}', false)
                 return
             end
         end
@@ -114,8 +126,7 @@ function base_show_full(io::IO, x::Type{T}) where {T<:Union{AbstractNode,Abstrac
         return
     end
 
-    # this type assert is behaving obscurely. When in Mill, it does not assert that LazyNode{T<:Symbol,D} where D is UnionAll, but in debugging using Debugger, it does
-    # x::UnionAll
+    x::UnionAll
     if Base.print_without_params(x)
         return show(io, Base.unwrap_unionall(x).name)
     end
@@ -137,12 +148,69 @@ function base_show_full(io::IO, x::Type{T}) where {T<:Union{AbstractNode,Abstrac
     print(io, " where ")
     show(io, x.var)
 end
+end
 
-function Base.show(io::IO, x::Type{T}) where {T<:Union{AbstractNode,AbstractMillModel}}
+@static if VERSION >= v"1.6.0-"
+function base_show_full_1_6(io::IO, @nospecialize(x::Type))
+    # basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
+    # specifically function show(io::IO, @nospecialize(x::Type))
+    if Base.print_without_params(x)
+        Base.show_type_name(io, Base.unwrap_unionall(x).name)
+        return
+    elseif get(io, :compact, true) && Base.show_typealias(io, x)
+        return
+    elseif x isa DataType
+        Base.show_datatype(io, x)
+        return
+    elseif x isa Union
+        if get(io, :compact, true)
+            Base.show_unionaliases(io, x)
+        else
+            print(io, "Union")
+            Base.show_delim_array(io, Base.uniontypes(x), '{', ',', '}', false)
+        end
+        return
+    end
+
+    x = x::UnionAll
+    wheres = TypeVar[]
+    let io = IOContext(io)
+        while x isa UnionAll
+            var = x.var
+            if var.name === :_ || Base.io_has_tvar_name(io, var.name, x)
+                counter = 1
+                while true
+                    newname = Symbol(var.name, counter)
+                    if !Base.io_has_tvar_name(io, newname, x)
+                        var = TypeVar(newname, var.lb, var.ub)
+                        x = x{var}
+                        break
+                    end
+                    counter += 1
+                end
+            else
+                x = x.body
+            end
+            push!(wheres, var)
+            io = IOContext(io, :unionall_env => var)
+        end
+        show(io, x)
+    end
+    Base.show_wheres(io, wheres)
+end
+end
+
+function Base.show(io::IO, @nospecialize(x::Type))
+    # println("_terseprint[] in Base.show: $(_terseprint[])")
     if _terseprint[]
         return base_show_terse(io, x)
     else
-        return base_show_full(io, x)
+        # VERSION < v"1.6.0-" returns true for 1.5.* and older, but for rc and beta of 1.6 it returns true
+        @static if VERSION < v"1.6.0-"
+            return base_show_full_1_5(io, x)
+        else
+            return base_show_full_1_6(io, x)
+        end
     end
 end
 
