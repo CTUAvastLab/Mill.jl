cb8a3f0b8a7f3753b43d8576b9a675785de0a9c2
Simon Mandlik <simon.mandlik@gmail.com>
2021-09-17 11:50:19 +0200

    requiring Zygote 0.6.21, broken NGramMatrix tests now functional again

--- test/ngram_matrix.jl
+++ test/ngram_matrix.jl
@@ -260,8 +260,7 @@ end
         A = randn(10, m)
         f = gradf(A -> sin.(A * B), A)
         df = gradf(A -> gradient(f, A)[1], A)
-        # see https://github.com/FluxML/Zygote.jl/issues/1067
-        @test_broken gradtest(df, A)
+        @test gradtest(df, A)
     end
 end
 
