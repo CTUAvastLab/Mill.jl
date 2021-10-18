3b819df6032a543329da4f0246604689e6640025
Simon Mandlik <simon.mandlik@gmail.com>
2021-10-18 12:32:30 +0200

    fixed on server doctests

--- src/datanodes/lazynode.jl
+++ src/datanodes/lazynode.jl
@@ -48,7 +48,7 @@ end
 ```jldoctest unpack2mill
 julia> LazyNode{:Sentence}(["foo bar", "baz"]) |> Mill.unpack2mill
 BagNode 	# 2 obs, 120 bytes
-  └── ArrayNode(2053×3 NGramMatrix with Int64 elements) 	# 3 obs, 176 bytes
+  └── ArrayNode(2053×3 NGramMatrix with Int64 elements) 	# 3 obs, 274 bytes
 ```
 
 See also: [`LazyNode`](@ref), [`LazyModel`](@ref).
