ca59365d7a4b6d2edbc31fa9958113d71b7e7f7b
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 09:46:20 +0000

    build based on 77e237c

--- dev/examples/musk/musk.jl
+++ dev/examples/musk/musk.jl
@@ -22,7 +22,7 @@ model(ds)
 
 opt_state = Flux.setup(Adam(), model);
 
-loss(m, x, y) = Flux.logitcrossentropy(m(x), y);
+loss(m, x, y) = Flux.Losses.logitcrossentropy(m(x), y);
 
 for e in 1:100
     if e % 10 == 1
