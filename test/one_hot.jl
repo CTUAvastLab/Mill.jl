7bc6269ea9bc98d7613d76a6ce2863a826598fd7
Šimon Mandlík <simon.mandlik@gmail.com>
2025-07-27 14:54:07 +0200

    fix: get rid of one hot array `hcat` override

--- test/one_hot.jl
+++ test/one_hot.jl
@@ -1,7 +1,5 @@
 @testset "onehot hcat optimizations" begin
     X1 = onehotbatch(collect(1:5), 1:10)
-    @test @which(hcat(X1, X1)).module ≡ OneHotArrays
-    @test @which(reduce(hcat, [X1, X1])).module ≡ Mill
 
     @test hcat(X1, X1) == onehotbatch(vcat(1:5, 1:5), 1:10)
     @test reduce(hcat, [X1, X1]) == onehotbatch(vcat(1:5, 1:5), 1:10)
